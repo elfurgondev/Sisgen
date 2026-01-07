1fa7178ad2cb8be6402ba2ec95737deba55f2a14
elfurgondev <elfurgondev@gmail.com>
2026-01-07 15:27:37 -0400

    born

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+import { createApp } from 'vue';
+// import App from './App.vue'; // We will create this later
+
+const app = createApp({});
+
+// Register global components here
+// app.component('example-component', ExampleComponent);
+
+app.mount('#app');
